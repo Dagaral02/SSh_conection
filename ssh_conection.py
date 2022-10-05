32d69f3fb4566c62bdf8c37572c6d58c579337d5
Dagaral02 <114167393+Dagaral02@users.noreply.github.com>
2022-10-05 21:16:02 +0200

    Add files via upload

--- ssh_conection.py
+++ ssh_conection.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-#ssh –i  keyprivadaXXxx.key_usuario –p 22 usuarioXXxx@192.168.50.1 “apt-get update && apt-upgrade && init 6”
-
+import subprocess
 import os
 
+me = subprocess.getoutput("whoami")
+me = me.strip()
 
 print("Hola,vamos a hacer una conexion remota para ejecutar los comandos que desees")
 
@@ -36,3 +37,39 @@ if(op == '1'):
             os.system(f"ssh -i {lugarpriv} -p {puerto} {user}@{ip} {command}") 
     elif(key == '2'):
         os.system(f"ssh {user}@{ip} {command}")
+elif(op == '2'):
+    
+    user = input("Dinos el usuario del equipo: ")
+    ip = input("Dinos la ip del servidor: ")
+
+    sshcopy = input("Importante esta copiada tu clave publica en el servidor, ¡Usuario!: ")
+
+    if(sshcopy == 'si'):
+        ls = input('Quieres hacer un ls en .ssh por si no te acuerdas de la Key: ')
+        if(ls == 'si'):
+            os.system(f'ls /home/{me}/.ssh')
+            lugarpriv =input ("Indica la ruta completa donde se encuentra la Key: " )
+            archcp = input ('Cual es la ruta incluida el archibo que quieres copiar: ')
+            wherecp = input ('Donde quieres poner el archivo que quieres copiar (pon la ruta entera): ')
+            os.system(f"scp -i {lugarpriv} {archcp} {user}@{ip}:{wherecp}")
+        elif(ls == 'no'):
+            lugarpriv =input ("Indica la ruta completa donde se encuentra la Key: " )
+            archcp = input ('Cual es la ruta incluida el archibo que quieres copiar: ')
+            wherecp = input ('Donde quieres poner el archivo que quieres copiar (pon l aruta entera): ')
+            os.system(f"scp -i {lugarpriv} {archcp} {user}@{ip}:{wherecp}") 
+    elif(sshcopy == 'no'): 
+        ls = input('Quieres hacer un ls en .ssh por si no te acuerdas de la Key: ')
+        if(ls == 'si'):
+            os.system(f'ls /home/{me}/.ssh')
+            lugarpub =input ("Indica la ruta completa donde se encuentra la Key: ")
+            os.system(f"ssh-copy-id -f -i {lugarpub} {user}@{ip}")
+            lugarpriv =input ("Indica la ruta completa donde se encuentra la Key: " )
+            archcp = input ('Cual es la ruta incluida el archibo que quieres copiar: ')
+            wherecp = input ('Donde quieres poner el archivo que quieres copiar (pon l aruta entera): ')
+            os.system(f"scp -i {lugarpriv} {archcp} {user}@{ip}:{wherecp}")
+        elif(ls == 'no'):
+            lugarpriv =input ("Indica la ruta completa donde se encuentra la Key: " )
+            archcp = input ('Cual es la ruta incluida el archibo que quieres copiar: ')
+            wherecp = input ('Donde quieres poner el archivo que quieres copiar (pon l aruta entera): ')
+            os.system(f"scp -i {lugarpriv} {archcp} {user}@{ip}:{wherecp}")
+    
